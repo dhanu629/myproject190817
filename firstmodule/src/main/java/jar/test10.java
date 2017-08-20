9cefb24c54ec473219dbc203fefd30e084969359
server <root@localhost.localdomain>
2017-08-20 05:05:45 -0700

    test10 file added

---
+++ firstmodule/src/main/java/jar/test10.java
@@ -0,0 +1,13 @@
+package jar;
+
+/**
+ * Hello world!
+ *
+ */
+public class test10
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World test10!" );
+    }
+}
